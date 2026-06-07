e66134f36215cba9a97cce94dad9922a9d43b8b6
Wajsar Josef <36114753+PepikVaio@users.noreply.github.com>
2026-06-07 22:17:27 +0200

    Create data.js

---
+++ apple_Send_to.../data.js
@@ -0,0 +1,39 @@
+window.addEventListener("load", () => {
+
+  function normalize(url) {
+    return url.split("?")[0]; // vezme jen základ bez parametrů
+  }
+
+  const mainImg = document.querySelector("[data-main='1']");
+  if (!mainImg) return;
+
+  const mainSrc = normalize(mainImg.getAttribute("src"));
+
+  document.querySelectorAll("img").forEach(img => {
+
+    if (img === mainImg) return;
+
+    const imgSrc = normalize(img.getAttribute("src"));
+
+    if (imgSrc === mainSrc) {
+      img.remove();
+    }
+
+  });
+
+});
+
+
+ /* Experimental */
+ window.addEventListener("load", () => {
+
+  const root = getComputedStyle(document.documentElement);
+  const bg = root.getPropertyValue("--background").trim();
+
+  const white = root.getPropertyValue("--set_White").trim();
+
+  if (bg !== white) {
+    // document.body.style.padding = "50px";
+  }
+
+});
